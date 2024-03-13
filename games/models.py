197b9468b1ae4a7b59167cc516fd9228257c9508
Angel Lockwood <angmarie0916@gmail.com>
2024-03-13 12:27:35 -0400

    added leaderboard model and listview

--- games/models.py
+++ games/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Leaderboard(models.Model):
+    username = models.TextField(max_length=20)
+    time = models.DateTimeField(auto_now_add=True)
+    game_type = models.TextField(max_length=20)
+    game_settings = models.TextField(max_length=20)
+    final_score = models.TextField(max_length=10)
+
+    def __str__(self):
+        return f"{self.username} - {self.final_score}"
--- games/views.py
+++ games/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Leaderboard
 
 class MathFactsView(TemplateView):
     template_name = "math-facts.html"
 
 class AnagramHuntView(TemplateView):
     template_name = "anagram-hunt.html"
+
+class LeaderboardListView(ListView):
+    model = Leaderboard
