9d420ea0a7c463a0b82588c89e4719831f4a2bdb
Angel Lockwood <angmarie0916@gmail.com>
2024-03-14 12:01:17 -0400

    added users and leaderboard to admin

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Leaderboard
+
+@admin.register(Leaderboard)
+class LeaderboardAdmin(admin.ModelAdmin):
+    model = Leaderboard
+    list_display = ['username', 'final_score', 'game_type', 'game_settings', 'time']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('time')
+
+        return ()
